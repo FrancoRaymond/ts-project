4d450168174e1fe3c9f5949ff6c06ae697fa77fb
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-10-02 17:40:43 +0200

    added react

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/ts-project/', // 👈 Replace with your GitHub repo name
+  plugins: [react()],
 })
